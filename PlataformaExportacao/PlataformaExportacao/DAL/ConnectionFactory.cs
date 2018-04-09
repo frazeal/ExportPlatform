b910be8c6c566e5cd84c30210345fecf42d112b3
frazeal <aaalves.francisco@gmail.com>
2018-04-08 21:48:51 -0300

    Add extraction from database using a procedure
    In this version:
    - extract data from database using a procedure
    - load data into a file (CSV, TXT)

---
+++ PlataformaExportacao/PlataformaExportacao/DAL/ConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaExportacao.DAL
+{
+    public class ConnectionFactory
+    {
+        public static IDbConnection CreateConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["CV4DB"];
+            IDbConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString.ConnectionString;
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ PlataformaExportacao/PlataformaExportacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using PlataformaExportacao.BLL;
+using PlataformaExportacao.BLL.VO;
+using PlataformaExportacao.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaExportacao
+{
+    class Program
+    {
+        private static IProcessable processingBO;
+        private static ProcessingDAO processingDAO = new ProcessingDAO();
+
+        static void Main(string[] args)
+        {
+            using (IDbConnection connection = ConnectionFactory.CreateConnection())
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("ERROR: an argument must be given.");
+                    return;
+                }
+                int jobId = Convert.ToInt32(args[0]);
+                ProcessingVO processingVO = processingDAO.Get(jobId);
+
+                processingBO = CreateProcessing(processingVO);
+                processingBO?.Process();
+            }
+        }
+
+        private static IProcessable CreateProcessing(ProcessingVO processingVO)
+        {
+            ProcessingTypeFactory.Initialize(processingVO);
+            return ProcessingTypeFactory.GetProcessing(processingVO);
+        }
+    }
+}
